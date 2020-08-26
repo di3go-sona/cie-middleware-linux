7937690f431a2a087825d57084a5093dd48f2194
di3go <diego.sonaglia@gmail.com>
2020-08-26 19:14:09 +0200

    added verificaPIN base

---
+++ CSP/CSP/verificaPIN.h
@@ -0,0 +1,12 @@
+//
+// Created by di3go on 26/08/2020.
+//
+
+#ifndef __VERIFICA_PIN_H__
+#define __VERIFICA_PIN_H__
+
+#include "../Util/defines.h"
+
+extern "C" int CALLBACK VerificaPIN( _In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPCSTR     lpCmdLine, _In_ int       nCmdShow);
+
+#endif
---
+++ Utils/verificapin.cpp
@@ -0,0 +1,6 @@
+#include "verificaPIN.h"
+
+int main()
+{
+	return VerificaPIN(nullptr, nullptr, nullptr, 0);
+}
